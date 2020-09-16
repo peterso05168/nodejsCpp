cad1deee30e9eb71d291bafabc1b0e1da694a448
Patrick So <pso@opm.com.hk>
2020-09-16 21:25:11 +0800

    added shoe logic for shared lib

--- app/index.ts
+++ app/index.ts
@@ -2,27 +2,40 @@ import express from "express";
 import * as http from "http";
 
 var bodyParser = require('body-parser');
-var greet = require("../build/Release/greet.node");
+var calOdds = require("../build/Release/calOdds.node");
 
 const app: express.Application = express();
 const server: http.Server = http.createServer(app);
 const port = 3000;
 
+let shoesMap = new Map();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+function addUsedCardToShoeByShoeId(shoeId:number, newUsedCards:Array<number>) {
+  let currentShoeUsedCards = shoesMap.get(shoeId);
+  if (currentShoeUsedCards) {
+    shoesMap.set(shoeId, currentShoeUsedCards.concat(newUsedCards));
+  }else {
+    shoesMap.set(shoeId, newUsedCards);
+  }
+}
+
 app.post('/api/calculateOdds', function(req, res) {
-  var usedCardsJsonStr = req.body.usedCards;
-  var usedCardsArray = JSON.parse(usedCardsJsonStr);
-  console.log(usedCardsArray);
+  var shoeDetailJsonStr = req.body.shoeDetail;
+  var shoeDetailObj = JSON.parse(shoeDetailJsonStr);
+  addUsedCardToShoeByShoeId(shoeDetailObj.shoeId, shoeDetailObj.usedCards);
+  console.log(shoesMap);
+  
   //TODO: will do calculation job on probability logic and return single interger.
-  res.status(200).send('Array received: ' + usedCardsJsonStr);
+  res.status(200).send('json received: '+ shoeDetailJsonStr);
 });
 
 app.get("/", (req: express.Request, res: express.Response) => {
   res
     .status(200)
-    .send(`Server running at port ${port}` + greet.greetHello("test"));
+    .send("testing");
 });
 server.listen(port, () => {
   console.log(`Server running at port ${port}`);
