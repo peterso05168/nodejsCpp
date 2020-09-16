520b8e1dff1a1bb3bb7443a4088460a8327e3c86
Patrick So <pso@opm.com.hk>
2020-09-16 19:58:29 +0800

    updated post

--- app/index.ts
+++ app/index.ts
@@ -1,10 +1,23 @@
 import express from "express";
 import * as http from "http";
+
+var bodyParser = require('body-parser');
 var greet = require("../build/Release/greet.node");
 
 const app: express.Application = express();
 const server: http.Server = http.createServer(app);
 const port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/api/calculateOdds', function(req, res) {
+  var usedCardsJsonStr = req.body.usedCards;
+  var usedCardsArray = JSON.parse(usedCardsJsonStr);
+  console.log(usedCardsArray);
+  //TODO: will do calculation job on probability logic and return single interger.
+  res.status(200).send('Array received: ' + usedCardsJsonStr);
+});
 
 app.get("/", (req: express.Request, res: express.Response) => {
   res
@@ -12,5 +25,5 @@ app.get("/", (req: express.Request, res: express.Response) => {
     .send(`Server running at port ${port}` + greet.greetHello("test"));
 });
 server.listen(port, () => {
-  console.log(`Server running at port ${port}` + greet.greetHello("test"));
+  console.log(`Server running at port ${port}`);
 });
